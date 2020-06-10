f0ef5f0a3a69f0a9e1438be3a601f4429108e12d
asdnyz <57964372+asdnyz@users.noreply.github.com>
2020-06-10 14:11:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  
+  //get location
+  navigator.geolocation.getCurrentPosition(success,error);
+  
+  function success(pos){
+    var lat = pos.coords.latitude;
+    var long = pos.coords.longitude;
+    weather(lat,long);
+  }
+  
+  function error(){
+    console.log('error');
+  }
+  
+  function weather(lat,long){
+    var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
+    
+    $.getJSON(URL, function(data){
+      console.log(data);
+      updateDOM(data);
+    });
+  }
+  
+  function updateDOM(data){
+    var city = data.name;
+	var country = data.sys.country;
+    var temp = Math.round(data.main.temp);
+    var desc = data.weather[0].description;
+    var icon = data.weather[0].icon;
+        
+    $('#city').html(city);
+	$('#country').html(country);
+    $('#temp').html(temp);
+    $('#desc').html(desc);
+    $('#icon').attr('src',icon);
+  }
+  
+});

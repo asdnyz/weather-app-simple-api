3a43da5412e647779ecddbad826cc26389083430
asdnyz <57964372+asdnyz@users.noreply.github.com>
2025-01-30 11:58:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-$(document).ready(function(){
-  
-  //get location
-  navigator.geolocation.getCurrentPosition(success,error);
-  
-  function success(pos){
-    var lat = pos.coords.latitude;
-    var long = pos.coords.longitude;
-    weather(lat,long);
-  }
-  
-  function error(){
-    console.log('error');
-  }
-  
-  function weather(lat,long){
-    var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
-    
-    $.getJSON(URL, function(data){
-      console.log(data);
-      updateDOM(data);
-    });
-  }
-  
-  function updateDOM(data){
-    var city = data.name;
-	var country = data.sys.country;
-    var temp = Math.round(data.main.temp);
-    var desc = data.weather[0].description;
-    var icon = data.weather[0].icon;
-        
-    $('#city').html(city);
-	$('#country').html(country);
-    $('#temp').html(temp);
-    $('#desc').html(desc);
-    $('#icon').attr('src',icon);
-  }
-  
+$(document).ready(function() {
+    // Geolocation to get user's location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            
+            // Fetch weather data based on the latitude and longitude
+            fetchWeatherData(lat, lon);
+        }, function(error) {
+            alert("Unable to retrieve your location.");
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+
+    // Function to fetch weather data from the API
+    function fetchWeatherData(lat, lon) {
+        const url = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`;
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+            success: function(data) {
+                // Update the UI with the fetched data
+                $('#city').text(data.name);
+                $('#country').text(data.sys.country);
+                $('#temp').text(data.main.temp.toFixed(1));
+                $('#desc').text(data.weather[0].description);
+                
+                // Set weather icon
+                const iconCode = data.weather[0].icon;
+                $('#icon').attr('src', `http://openweathermap.org/img/wn/${iconCode}.png`);
+            },
+            error: function() {
+                // If the API call fails, show an error message
+                alert("Error fetching weather data. Please try again.");
+            }
+        });
+    }
 });
